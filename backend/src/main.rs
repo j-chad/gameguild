6f8aba00b27990ac8655e83da19706e9480277b5
Jackson <contact@jacksonc.dev>
2025-04-23 21:35:05 +1200

    scaffold

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,15 @@
+mod app;
+mod config;
+mod db;
+mod error;
+mod state;
+mod routes;
+mod handlers;
+mod models;
+mod schemas;
+mod services;
+mod utils;
+
 fn main() {
     println!("Hello, world!");
 }
